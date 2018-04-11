bc400c64d835171418fa23e624fcddb796bf1eea
domin <dominik.gulczynski@gmail.com>
2018-04-11 22:02:11 +0200

    PO - Lab 7 - implementing serialization

--- 2017-2018/Programowanie Obiektowe/7/src/KontrolkaSamochodu.java
+++ 2017-2018/Programowanie Obiektowe/7/src/KontrolkaSamochodu.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class KontrolkaSamochodu extends JComponent {
     Samochod samochod;
@@ -7,7 +11,8 @@ public class KontrolkaSamochodu extends JComponent {
     public KontrolkaSamochodu(Samochod samochod) {
         this.samochod = samochod;
 
-        this.setLayout(new GridLayout(2,1 ));
+        this.setLayout(new BorderLayout());
+        this.add(new JLabel("Kontrolka Samochodu"), BorderLayout.NORTH);
 
         JPanel panelSamochodu = new JPanel();
         panelSamochodu.setLayout(new GridLayout(4,2));
@@ -23,14 +28,53 @@ public class KontrolkaSamochodu extends JComponent {
         panelSamochodu.add(textRocznik);
         panelSamochodu.add(new JLabel("Rejestracja:"));
         panelSamochodu.add(textRejestracja);
-        this.add(panelSamochodu);
+        this.add(panelSamochodu, BorderLayout.CENTER);
 
         JPanel panelPrzyciskow = new JPanel();
         panelPrzyciskow.setLayout(new GridLayout(2,2));
+        JTextField textPlik = new JTextField("samochod.ser");
         JButton buttonZapis = new JButton("Zapisz");
         JButton buttonOdczyt = new JButton("Wczytaj");
+        buttonZapis.addActionListener(e->{
+            samochod.model = textModel.getText();
+            samochod.kolor = textKolor.getText();
+            samochod.rocznik = Integer.parseInt(textRocznik.getText());
+            samochod.rejestracja = textRejestracja.getText();
+            String filename = textPlik.getText();
+            if(!filename.equals("")){
+                try {
+                    ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename));
+                    out.writeObject(samochod);
+                    out.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        buttonOdczyt.addActionListener(e->{
+            String filename = textPlik.getText();
+            if(!filename.equals("")) {
+                try {
+                    ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+                    Samochod temp = (Samochod) in.readObject();
+                    samochod.model = temp.model;
+                    samochod.kolor = temp.kolor;
+                    samochod.rocznik = temp.rocznik;
+                    samochod.rejestracja = temp.rejestracja;
+                    textModel.setText(temp.model);
+                    textKolor.setText(temp.kolor);
+                    textRocznik.setText(String.valueOf(temp.rocznik));
+                    textRejestracja.setText(temp.rejestracja);
+                    in.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        panelPrzyciskow.add(new Label("Nazwa pliku:"));
+        panelPrzyciskow.add(textPlik);
         panelPrzyciskow.add(buttonZapis);
         panelPrzyciskow.add(buttonOdczyt);
-        this.add(panelPrzyciskow);
+        this.add(panelPrzyciskow, BorderLayout.SOUTH);
     }
 }
--- 2017-2018/Programowanie Obiektowe/7/src/Main.java
+++ 2017-2018/Programowanie Obiektowe/7/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Samochod samochod = new Samochod("Seat Ibiza", "Czerwony", 2016, "XX 2345");
 
         //Create and set up the window.
-        JFrame frame = new JFrame("Swingtest");
+        JFrame frame = new JFrame("Kontrolki");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setLayout(new BorderLayout());
