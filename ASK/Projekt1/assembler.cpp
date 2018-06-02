e9bfe675709d7391a22e1b2fd7d8e38eb86418ed
domin <dominik.gulczynski@gmail.com>
2018-06-02 08:17:57 +0200

    ASK - MIPS registers info added

--- ASK/Projekt1/assembler.cpp
+++ ASK/Projekt1/assembler.cpp
@@ -75,6 +75,11 @@ class InstructionParser {
                 imap[name] = new Instruction(s);
             }
             file.close();
+            /// mapa reg->binary
+            string reg[32]={"$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k0", "$gp", "$sp", "$fp", "$ra"};
+            string bin[32]={"00000", "00001", "00010", "00011", "00100", "00101", "00110", "00111", "01000", "01001", "01010", "01011", "01100", "01101", "01110", "01111", "10000", "10001", "10010", "10011", "10100", "10101", "10110", "10111", "11000", "11001", "11010", "11011", "11100", "11101", "11110", "11111"};
+            for(int i = 0; i<32; i++)
+                rmap[reg[i]]=bin[i];
         }
         void parseInstruction(string input) {
         vector < string > vec;
@@ -117,11 +122,12 @@ class InstructionParser {
         return imap[name];
     }
     private:
-        map<string, Instruction * > imap;
+        map<string, Instruction*> imap;
+        map<string, string> rmap
 };
 
 int main() {
-    InstructionParser IP("instrukcje.txt");
+    InstructionParser IP("instructions.txt");
     Instruction * I = IP.get("add");
     IP.parseInstruction("add 1 10 1000 #test komentarza");
     IP.parseInstruction("sub 1 10 101\tkomentarz");
