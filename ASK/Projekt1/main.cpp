0204f2832ab2c19c9702d0d52c8cadd702ef5b42
Domingo1337 <dominik.gulczynski@gmail.com>
2018-06-02 22:40:00 +0200

    ASK - MIPS one more time

--- ASK/Projekt1/main.cpp
+++ ASK/Projekt1/main.cpp
@@ -5,6 +5,6 @@ int main(int argc, char **argv){
         std::cout << "Invalid arguments.\nUse: <./main <filename>> instead.\n";
         return EXIT_FAILURE;
     }
-    InstructionParser IP("instrukcje.txt");
+    InstructionParser IP("instructions.dat");
     IP.parseFile(argv[1]);
 }
