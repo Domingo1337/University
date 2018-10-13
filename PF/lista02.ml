f2c86accfc107526ee6859f455cc3d579017df98
Domingo1337 <dominik.gulczynski@gmail.com>
2018-10-13 23:06:35 +0200

    PF - Lista 2 update

--- PF/lista02.ml
+++ PF/lista02.ml
@@ -1,14 +1,27 @@
-(* Zadanie 1 *)
+(* Utilities *)
 let rec map (f: 'a -> 'b) (l: 'a list) =
   match l with
   | [] -> []
   | h::t -> (f h)::(map f t);;
 
+let rec length (l: 'a list) =
+  match l with
+  | [] -> 0
+  | h::t -> 1 + (length t);;
+
+let reverse (l: 'a list) =
+  let rec iter l1 l2 =
+    match l1, l2 with
+    | [], l2 -> l2;
+    | (h1::t1), l2 -> iter t1 (h1::l2)
+  in iter l [];;
+
+(* Zadanie 1 *)
 let rec sublists (l: 'a list) =
   match l with
   | [] -> [[]]
-  | h::t -> let tails = sublists t in
-            (map (fun lst -> h::lst) tails)@tails;;
+  | h::t -> let tails = sublists t
+            in (map (fun lst -> h::lst) tails)@tails;;
 
 (* Zadanie 2  *)
 let rec take_nth (l: 'a list) (n: int) =
@@ -16,18 +29,13 @@ let rec take_nth (l: 'a list) (n: int) =
   | [] -> failwith "called with n bigger than l's length"
   | h::t ->  if n = 0 then h else take_nth t (n-1);; 
 
-let rec length (l: 'a list) = 
-  match l with
-  | [] -> 0 
-  | h::t -> 1 + (length t);; 
-
-(* to jest jakas tragedia ten cycle ale nie wiem jak to lepiej zrobic *)
+(* jesli b = a n to b[i] = a[i+n mod len(a)] *)
 let cycle (l: 'a list) (n:int) =
-  let rec len = length l 
-      and aux (i: int) = 
-        if i < len then (take_nth l ((i-n+len) mod len))::(aux (i+1))
-        else [] in
-  aux 0;;
+  let reclen = length l
+  in let rec iter (i: int) =
+      if i < len then (take_nth l ((i-n+len) mod len))::(iter (i+1))
+      else []
+     in iter 0;;
 
 (* Zadanie 3 *)
 let rec merge (cmp: 'a -> 'a -> bool) (l1: 'a list) (l2: 'a list) =
@@ -36,59 +44,59 @@ let rec merge (cmp: 'a -> 'a -> bool) (l1: 'a list) (l2: 'a list) =
   | lst, [] -> lst
   | (h1::t1), (h2::t2) -> if cmp h1 h2 then h1::(merge cmp t1 l2) else h2::(merge cmp l1 t2);;
 
-
-let reverse (l: 'a list) =
-  let rec aux l1 l2
+let rec merge_t (cmp: 'a -> 'a -> bool) (l1: 'a list) (l2: 'a list) =
+  let rec iter l1 l2 m =
     match l1, l2 with
-    | [], l2 -> l2;
-    | (h1::t1), l2 -> aux t1 (h1::l2)
-  in aux l [];; 
+    | [], lst
+    | lst, [] -> (reverse m)@lst
+    | (h1::t1), (h2::t2) -> if cmp h1 h2 then iter t1 l2 (h1::m) else iter l1 t2 (h2::m)
+  in iter l1 l2 [];;
 
-let rec merge_t (cmp: 'a -> 'a -> bool) (l1: 'a list) (l2: 'a list) (m: 'a list) =
-  match l1, l2 with
-  | [], [] -> reverse m
-  | [], (h::t)
-  | (h::t), [] -> merge_t cmp [] t (h::m)
-  | (h1::t1), (h2::t2) -> if cmp h1 h2 then merge_t cmp t1 l2 (h1::m) else merge_t cmp l1 t2 (h2::m);;
-
-let rec take (l: 'a list) (start: int) (stop: int) =
-  match l, stop with
-  | _, -1 -> []
-  | [], _ -> failwith "empty list"
-  | (h::t), _ -> if start = 0 then h::(take t 0 (stop-1)) else take t (start-1) (stop-1);; 
+(* splits list in two lists with indices: 0 to n (reversed) and n+1 to the last *)
+let split (l: 'a list) (n: int) =
+  let rec iter l1 l2 n =
+    match l2, n with
+    | [], _
+    | _, -1 -> l1, l2
+    | (h::t), _ -> iter (h::l1) t (n-1)
+  in iter [] l n;;
 
 let rec merge_sort (cmp: 'a -> 'a -> bool) (l: 'a list) (n: int) =
-  if n = 0 then l else merge_t cmp (merge_sort cmp (take l 0     (n/2)) (n/2))
-                                   (merge_sort cmp (take l ((n/2)+1) n) (n-(n/2)-1)) [];;
+  if n = 0 then l
+  else match split l (n/2) with
+       | lft, rgt -> merge_t cmp (merge_sort cmp lft (n/2))
+                                 (merge_sort cmp rgt (n-(n/2)-1));;
 
 let rec merge_srt (l: int list) =
   merge_sort (<=) l ((length l) -1);;
 
-let rec merge_rev (cmp: 'a -> 'a -> bool) (l1: 'a list) (l2: 'a list) (m: 'a list) =
-  match l1, l2 with
-  | [], [] -> m
-  | [], (h::t)
-  | (h::t), [] -> merge_rev cmp [] t (h::m)
-  | (h1::t1), (h2::t2) -> if cmp h1 h2 then merge_rev cmp t1 l2 (h1::m) else merge_rev cmp l1 t2 (h2::m);;
+let rec merge_rev (cmp: 'a -> 'a -> bool) (l1: 'a list) (l2: 'a list) =
+  let rec iter l1 l2 m =
+    match l1, l2 with
+    | [], [] -> m
+    | [], (h::t)
+    | (h::t), [] -> iter [] t (h::m)
+    | (h1::t1), (h2::t2) -> if cmp h1 h2 then iter t1 l2 (h1::m) else iter l1 t2 (h2::m)
+  in iter l1 l2 [];;
 
 let rec merge_rsort (cmp: 'a -> 'a -> bool) (l: 'a list) (n: int) =
-  if n = 0 then l else merge_rev cmp (merge_rsort cmp (take l 0 (n/2)) (n/2)) (merge_rsort cmp (take l ((n/2)+1) n) (n-(n/2)-1)) [];;
+  if n = 0 then l
+  else let invcmp = (fun a b -> not(cmp a b))
+       in match split l (n/2) with
+          | lft, rgt ->  merge_rev invcmp (merge_rsort invcmp lft (n/2))
+                                          (merge_rsort invcmp rgt (n-(n/2)-1));;
 
 let merge_rsrt (l: int list) =
   merge_rsort (<=) l ((length l)-1);;
 
-(* dokleja do acc liczby naturalne od 0 do n w kolejnosci rosnacej*)
-let rec ints (n : int) (acc: int list) =
-  if n = 0 then n::acc else ints (n-1) (n::acc);;
-
 (* Zadanie 4 *)
 let partition (pred: 'a -> bool) (l: 'a list) = 
-  let rec aux lst passed failed =
+  let rec iter lst passed failed =
     match lst with
     | [] -> (passed, failed)
-    | (h::t) -> if pred h then aux t (h::passed) failed
-                          else aux t passed (h::failed)
-  in aux l [] [];;
+    | (h::t) -> if pred h then iter t (h::passed) failed
+                          else iter t passed (h::failed)
+  in iter l [] [];;
 
 let rec quick_sort (cmp: 'a -> 'a -> bool) (l: 'a list) =
   match l with
@@ -100,8 +108,8 @@ let rec quick_sort (cmp: 'a -> 'a -> bool) (l: 'a list) =
 let rec input (a :'a) (l: 'a list) =
   match l with
   | [] -> [[a]]
-  | h::t -> let tails = input a t in
-            (a::h::t)::(map (fun lst -> h::lst) tails);;
+  | h::t -> let tails = input a t
+            in (a::l)::(map (fun lst -> h::lst) tails);;
 
 let rec concat (l: 'a list list) =
   match l with 
@@ -111,21 +119,20 @@ let rec concat (l: 'a list list) =
 let rec perms (l: 'a list) =
   match l with
   | [] -> [[]]
-  | h::t -> let tails = perms t in
-            concat (map (fun lst -> input h lst) tails);;
+  | h::t -> let tails = perms t
+            in concat (map (fun lst -> input h lst) tails);;
 
 (* Zadanie 6 *)
 let prefixes (l: 'a list) =
-  let rec aux r acc =
+  let rec iter r acc =
     match r with
     | [] -> r::acc
-    | (h::t) -> match acc with 
-                | (ha::ta) -> aux t (h::(map (fun l -> h::l) acc))
-  in aux (reverse l) [[]];;
+    | (h::t) ->  iter t ([h]::(map (fun l -> h::l) acc))
+  in iter (reverse l) [];;
 
 let suffixes (l: 'a list) =
-  let rec aux lst acc =
+  let rec iter lst acc =
     match lst with
-    | [] -> acc
-    | (h::t) -> aux t (lst::acc);;
-  in aux l [[]];;
+    | [] -> []::acc
+    | (h::t) -> iter t (lst::acc)
+  in iter l [];;
