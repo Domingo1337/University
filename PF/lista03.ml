ab954b90dc232345590893cbeb80964245bb964f
Dominik Gulczyński <dominik.gulczynski@nokia.com>
2018-10-29 13:09:54 +0100

    PF - Lista 3 update

--- PF/lista03.ml
+++ PF/lista03.ml
@@ -30,26 +30,57 @@ let rec fold_right_a (f: 'a -> 'b -> 'b) (xs: 'a list) (c: 'b) =
 let rec fold_right_b (f: 'a -> 'b -> 'b) (xs: 'a list) (c: 'b) =
   List.fold_left (fun b a -> f a b) c (List.rev xs);;
 
-
-let map f xs = fold_right_a (fun x xs -> f x :: xs) xs [];;
-
 (* Zadanie 4 *)
+(* jawna rekursja *)
+let rec input (y: 'a) (xs: 'a list) =
+  match xs with
+  | [] -> [[y]]
+  | h::t -> (y::xs)::(List.map (fun xs -> h::xs) (input y t));;
+
+(* jawna rekursja ogonowa *)
+let input_t (y: 'a) (xs: 'a list) =
+  let rec aux xs acc =
+    match xs with
+    | [] -> List.map List.rev (fst acc) (* przy liczeniu permutacji rev chyba nie będzie potrzebny *)
+    | h::t -> let xs = h::(snd acc)
+              in aux t ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs)
+  in aux xs ([[y]], []);;
+
+(* fold na podstawie ogonowej *) (* Wyniki sa odwrocone ale to nas nie boli przy liczeniu permutacji*) 
+let ins_everywhere_a (y: 'a) (xs: 'a list) =
+  fst (List.fold_right (fun h acc-> let xs = h::(snd acc)
+                                    in ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs))
+                       xs 
+                       ([[y]], []))
+
+let ins_everywhere_b (y: 'a) (xs: 'a list) =
+  fst (List.fold_left (fun acc h-> let xs = h::(snd acc) 
+                                   in ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs))
+                      ([[y]], [])
+                      xs)
 
 (* Zadanie 5 *)
-let rec polynomial_5a (p: float list) (x: float) =
+let rec polynomial_a (p: float list) (x: float) =
   match p with
   | [] -> 0.
   | h::t -> h+.x*.(polynomial_5a t x);;
 
-let polynomial_5b (p: float list) (x: float) =
+let polynomial_b (p: float list) (x: float) =
   List.fold_right (fun h acc -> h+.x*.acc) p 0.;;
 
-let polynomial_5c (p: float list) (x: float) =
+let polynomial_c (p: float list) (x: float) =
   let rec aux p y acc =
     match p with
     | [] -> acc
     | h::t -> aux t (x*.y) (acc +. y*.h)
   in aux p 1. 0.;;
 
-let polynomial_5d (p: float list) (x: float) =
+let polynomial_d (p: float list) (x: float) =
   List.fold_left (fun acc h -> h+.x*.acc) 0. p;;
+
+(* Zadanie 6 *)
+let perms_a (xs: 'a list) =
+  List.fold_left (fun acc x -> List.concat (List.map (ins_everywhere_a x) acc)) [[]] xs;;
+
+let perms_b (xs: 'a list) =
+  List.fold_right (fun x acc -> List.concat (List.map (ins_everywhere_a x) acc)) xs [[]];;
