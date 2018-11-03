aed9983bab197d7f53feed96c6934e22aba6d0d6
Domingo1337 <dominik.gulczynski@gmail.com>
2018-11-03 15:48:38 +0100

    PF - lista 03/04

--- PF/lista03.ml
+++ PF/lista03.ml
@@ -1,101 +1,108 @@
 (* Zadanie 1 *)
 let atoi_a (xs: char list) =
-  let rec aux xs acc = 
+  let rec aux xs acc =
     match xs with
     | [] -> acc
     | x::xs -> aux xs (10*acc + (int_of_char x) - 48)
-  in aux xs 0;;
+  in aux xs 0
 
 let atoi_b (xs: char list) =
-  List.fold_left (fun acc c -> 10*acc + (int_of_char c) - 48) 0 xs;;
+  List.fold_left (fun acc c -> 10*acc + (int_of_char c) - 48) 0 xs
 
 (* Zadanie 2 *)
 let polynomial_a (p: float list) (x: float) =
   let rec aux p acc =
-    match p with 
+    match p with
     | [] -> acc
     | [h] -> acc +. h
     | h::t -> aux t ((acc+.h) *. x)
-  in aux p 0.0;; 
+  in aux p 0.0
 
 let polynomial_b (p: float list) (x: float) =
-  List.fold_left (fun acc h -> acc*.x +. h) 0. p;;
+  List.fold_left (fun acc h -> acc*.x +. h) 0. p
 
 (* Zadanie 3 *)
 let rec fold_right_a (f: 'a -> 'b -> 'b) (xs: 'a list) (c: 'b) =
   match xs with
   | [] -> c
-  | x::xs -> f x (fold_right_a f xs c);;
+  | x::xs -> f x (fold_right_a f xs c)
 
 let rec fold_right_b (f: 'a -> 'b -> 'b) (xs: 'a list) (c: 'b) =
-  List.fold_left (fun b a -> f a b) c (List.rev xs);;
+  List.fold_left (fun b a -> f a b) c (List.rev xs)
 
 (* Zadanie 4 *)
 (* jawna rekursja *)
 let rec input (y: 'a) (xs: 'a list) =
   match xs with
   | [] -> [[y]]
-  | h::t -> (y::xs)::(List.map (fun xs -> h::xs) (input y t));;
+  | h::t -> (y::xs)::(List.map (fun xs -> h::xs) (input y t))
 
 (* jawna rekursja ogonowa *)
 let input_t (y: 'a) (xs: 'a list) =
   let rec aux xs acc =
     match xs with
-    | [] -> List.map List.rev (fst acc) (* przy liczeniu permutacji rev chyba nie będzie potrzebny *)
+    | [] -> List.map List.rev (fst acc) (* przy liczeniu permutacji rev nie będzie potrzebny *)
     | h::t -> let xs = h::(snd acc)
-      in aux t ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs)
-  in aux xs ([[y]], []);;
+              in aux t ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs)
+  in aux xs ([[y]], [])
 
-(* fold na podstawie ogonowej *) (* Wyniki sa odwrocone ale to nas nie boli przy liczeniu permutacji*) 
+(* fold na podstawie ogonowej *)
+(* Wyniki sa odwrocone ale to nas nie boli przy liczeniu permutacji*)
 let ins_everywhere_a (y: 'a) (xs: 'a list) =
   fst (List.fold_right (fun h acc-> let xs = h::(snd acc)
-                         in ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs))
-         xs 
-         ([[y]], []))
+                                    in ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs))
+                       xs
+                       ([[y]], []))
 
 let ins_everywhere_b (y: 'a) (xs: 'a list) =
-  fst (List.fold_left (fun acc h-> let xs = h::(snd acc) 
-                        in ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs))
-         ([[y]], [])
-         xs)
+  fst (List.fold_left (fun acc h-> let xs = h::(snd acc)
+                                   in ((y::xs)::(List.map (fun ys -> h::ys) (fst acc)), xs))
+                      ([[y]], [])
+                      xs)
 
 (* Zadanie 5 *)
 let rec polynomial_a (p: float list) (x: float) =
   match p with
   | [] -> 0.
-  | h::t -> h+.x*.(polynomial_a t x);;
+  | h::t -> h+.x*.(polynomial_a t x)
 
 let polynomial_b (p: float list) (x: float) =
-  List.fold_right (fun h acc -> h+.x*.acc) p 0.;;
+  List.fold_right (fun h acc -> h+.x*.acc) p 0.
 
 let polynomial_c (p: float list) (x: float) =
   let rec aux p y acc =
     match p with
     | [] -> acc
     | h::t -> aux t (x*.y) (acc +. y*.h)
-  in aux p 1. 0.;;
+  in aux p 1. 0.
 
 let polynomial_d (p: float list) (x: float) =
-  List.fold_left (fun acc h -> h+.x*.acc) 0. p;;
+  List.fold_left (fun acc h -> h+.x*.acc) 0. p
 
 (* Zadanie 6 *)
 let perms_a (xs: 'a list) =
-  List.fold_left (fun acc x -> List.concat (List.map (ins_everywhere_a x) acc)) [[]] xs;;
+  List.fold_left (fun acc x -> List.concat (List.map (ins_everywhere_a x) acc)) [[]] xs
 
 let perms_b (xs: 'a list) =
-  List.fold_right (fun x acc -> List.concat (List.map (ins_everywhere_a x) acc)) xs [[]];;
+  List.fold_right (fun x acc -> List.concat (List.map (ins_everywhere_a x) acc)) xs [[]]
 
 (* Zadanie 7 *)
 let ins_ord_a (comp: 'a -> 'a -> bool) (y: 'a) (xs: 'a list) =
-  let (b, xs) = List.fold_right (fun x (b, xs) -> if b && comp x y then (false, x::(y::xs)) else (b, x::xs)) xs (true, [])
-  in if b then y::xs else xs;;
+  let (b, xs) = List.fold_right (fun x (b, xs) -> if b && comp x y then (false, x::(y::xs))
+                                                  else (b, x::xs))
+                                xs
+                                (true, [])
+  in if b then y::xs else xs
 
 let ins_ord_b (comp: 'a -> 'a -> bool) (y: 'a) (xs: 'a list) =
-  let (b, xs) = List.fold_left (fun (b, xs) x  -> if b && comp y x then (false, x::(y::xs)) else (b, x::xs)) (true, []) xs
-  in if b then List.rev (y::xs) else List.rev xs;;
+  let (b, xs) = List.fold_left (fun (b, xs) x  -> if b && comp y x then (false, x::(y::xs))
+                               else (b, x::xs))
+                               (true, [])
+                               xs
+  in if b then List.rev (y::xs) else List.rev xs
 
 let isort_a (comp: 'a -> 'a -> bool) (xs: 'a list) =
-  List.fold_right (ins_ord_a comp) xs [];;
+  List.fold_right (ins_ord_a comp) xs []
 
 let isort_b (comp: 'a -> 'a -> bool) (xs: 'a list) =
-  List.fold_right (ins_ord_a comp) xs [];;
+  List.fold_right (ins_ord_a comp) xs []
--- PF/lista04.ml
+++ PF/lista04.ml
@@ -1,55 +1,125 @@
 type 'a mtx = 'a list list
 exception Mtx of string
 
+(* Narzedzia *)
 let mtx_id (size: int) (zero: 'a) (one: 'a) =
   let rec id_row n j acc =
     if j < size then id_row (n-1) (j+1) ((if n = 0 then one else zero)::acc)
-                else acc
+    else acc
   and aux i acc =
-    if i < size then aux (i+1) ((id_row i 0 [])::acc) else acc
-  in aux 0 [];;
+    if i < size then aux (i+1) ((id_row i 0 [])::acc)
+    else acc
+  in aux 0 []
 
-(* Zadanie 1 *)
+
+let print_mtx (m: 'a mtx) (print_a: 'a -> unit) =
+  List.iter (fun row -> print_string"[";
+                        List.iter (fun a -> print_a a; print_string " ") row;
+                        print_string "]\n")
+            m;
+  print_string "\n"
+
+let print_fl_mtx (m: float mtx) =
+  print_mtx m print_float
+
+(* Zadanie 8 *)
 let mtx_dim (m: 'a mtx)=
   match m with
-  | [] -> raise (Mtx "Pusta macierz")
+  | [] -> raise (Mtx "mtx_dim called with empty matrix")
   | m -> List.fold_left (fun (rows, cols) current -> let len = List.length current in
-                                              if len < 1 then raise (Mtx "Macierz ma pusty wiersz")
-                                              else if cols = -1 then (1, len)
-                                              else if len==cols then ((rows+1), cols)
-                                              else raise (Mtx "Macierz ma wiersz o niepoprawnej długości"))
-                        (-1,-1)
-                        m;;
+                          if len < 1 then raise (Mtx "mtx_dim encountered empty row")
+                          else if cols = -1 then (1, len)
+                          else if len==cols then ((rows+1), cols)
+                          else raise (Mtx "mtx_dim encountered row of invalid length"))
+           (-1,-1)
+           m
 
-(* Zadanie 2 *)
-let mtx_row (row: int) (m: 'a mtx) = 
+(* Zadanie 9 *)
+let mtx_row (row: int) (m: 'a mtx) =
   let rec aux m i =
     match m with
-    | [] -> raise (Mtx "Nie ma takiego wiersza")
-    | r::rs -> if i = 1 then r else aux rs (i-1)
-  in aux m row;;
+    | [] -> raise (Mtx "mtx_row called with index bigger than matrix height")
+    | r::rs -> if i = 1 then r
+               else aux rs (i-1)
+  in aux m row
 
-let rec take (elem: int) (row: 'a list) =
-  match row with
-  | [] -> raise (Mtx "Pusty wiersz!")
-  | r::rs -> if elem = 1 then r else take (elem-1) rs;;
+(* funkcja pomocnicza wybierajaca i-ty element listy *)
+let rec take (i: int) (lst: 'a list) =
+  match lst with
+  | [] -> raise (Mtx "take called with i bigger than lst length")
+  | h::t -> if i = 1 then h
+            else take (i-1) t
 
 let mtx_column (column: int) (m: 'a mtx) =
-  List.fold_right (fun row acc-> (take column row)::acc) m [];;
+  List.fold_right (fun row acc-> (take column row)::acc) m []
 
 let rec mtx_elem (column: int) (row: int) (m: 'a mtx) =
   match m with
-  | [] -> raise (Mtx "Nie ma takiego wiersza")
+  | [] -> raise (Mtx "mtx_elem called with row bigger than matrix height")
   | r::rs -> if row = 1 then take column r
-             else mtx_elem column (row-1) rs;;
+             else mtx_elem column (row-1) rs
 
+(* Zadanie 10 *)
 let rec transpose (m: 'a mtx) =
-  let rec aux t m =
+  let rec aux m =
     match m with
-    | [] -> t
-    | m -> let (t1, m1) = List.fold_right (fun row (t, m) -> match row with 
-                                                   |[] -> (t,m)
-                                                   |elem::elems -> (elem::t, elems::m)) 
-                                          m ([], [])
-           in aux t1 m1
-  in aux [] m;;
+    | [] -> []
+    | h::t -> let (t1, m1) =
+                List.fold_right
+                  (fun row (t, m) -> match row with
+                                     | [] -> (t,m)
+                                     | [elem] -> (elem::t, m)
+                                     | elem::elems -> (elem::t, elems::m))
+                  m ([], [])
+              in t1::(aux m1)
+  in aux m
+
+(* Zadanie 11 *)
+let mtx_map2 (f: 'a -> 'a -> 'b) (m1: 'a mtx) (m2: 'a mtx) =
+  List.map2 (fun r1 r2 -> List.map2 (fun a1 a2 -> f a1 a2) r1 r2) m1 m2
+
+let mtx_add =
+  mtx_map2 (+)
+
+(* Zadanie 12 *)
+type 'a vector = 'a list
+
+let scalar_prod (v1: float vector) (v2: float vector) =
+  List.fold_left2 (fun acc f1 f2 -> acc +. f1 *. f2) 0.0 v1 v2
+
+let polynomial (p: float list) (x: 'float) =
+  let rec make_xs l =
+    match l with
+    | [] -> []
+    | [_] -> [1.]
+    | h::t -> let tail = make_xs t in (x *. (List.hd tail))::tail
+  in scalar_prod p (make_xs p);;
+
+(* Zadanie 13 *)
+let mtx_apply (m: float mtx) (v: float vector) =
+  List.map (scalar_prod v) m
+
+(* mnozenie macierzy m1 m2 to mnozenie m1 przez wektory m2, ktore otrzymujemy jako liste przez transponowanie m2 *)
+let mtx_mul (m1: float mtx) (m2: float mtx) =
+  List.map (mtx_apply (transpose m2)) m1
+
+(* Zadanie 14 *)
+
+(* funkcja pomocniczna do obliczania minorow *)
+(* odrzuca i-ty element listy *)
+let rec filter_i_out (l: 'a list) (i: 'int) =
+  match l with
+  | [] -> []
+  | h::t -> if i = 0 then filter_i_out t (-1) else h::(filter_i_out t (i-1))
+
+(* obliczany za pomocą rozwinięcia Laplace'a *)
+(* nie warto uruchamiac dla macierzy o rozmiarze > 10 *)
+let rec det (m: 'float mtx) =
+  match m with
+  | [] -> 0.
+  | [[x]] -> x
+  (* korzystamy z tego że det M == det M^T wiec obliczamy transponowane minory zeby latwo usunac kolumne (wiersz) *)
+  | row::rows -> let columns = transpose rows in
+    fst (List.fold_left (fun (acc, i) elem -> (elem *. (det (filter_i_out columns i)) +. acc, (i+1)))
+           (0.,0)
+           row)
