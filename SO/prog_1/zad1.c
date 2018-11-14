0097e4e1113da56d1cf9d036bce70ecd2c810594
Domingo1337 <dominik.gulczynski@gmail.com>
2018-11-14 21:01:49 +0100

    SO - Pracownia 1 - treść zadań i szkielety rozwiązań

---
+++ SO/prog_1/zad1.c
@@ -0,0 +1,23 @@
+/* Imię nazwisko: Maksymilian Debeściak
+ * Numer indeksu: 999999
+ *
+ * Oświadczam, że:
+ *  - rozwiązanie zadania jest mojego autorstwa,
+ *  - jego kodu źródłowego dnie będę udostępniać innym studentom,
+ *  - a w szczególności nie będę go publikować w sieci Internet.
+ *
+ * Q: Zdefiniuj proces "zombie".
+ * A: ...
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void) {
+  return EXIT_SUCCESS;
+}
