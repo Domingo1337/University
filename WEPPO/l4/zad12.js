16345afb6419f3619192992e673f342107e5b835
Domingo1337 <dominik.gulczynski@gmail.com>
2018-11-21 19:49:43 +0100

    WEPPO - l4 update

--- WEPPO/l4/zad12.js
+++ WEPPO/l4/zad12.js
@@ -1,24 +1,26 @@
-// 1
 var Tree = function (left, value, right) {
-    this.left = left
-    this.value = value
-    this.right = right
+    return {
+        left: left,
+        value: value,
+        right: right,
+        [Symbol.iterator]: function* () {
+            function* iter_tree(tree) {
+                if (tree.left != null)
+                    yield* iter_tree(tree.left)
+
+                yield tree.value
+
+                if (tree.right != null)
+                    yield* iter_tree(tree.right)
+            }
+            yield* iter_tree(this)
+        }
+    }
 }
-var root = new Tree(new Tree(new Tree(null, 1, null), 2, null), 5, null)
+
+var root = new Tree(new Tree(new Tree(null, 1, null), 2, null), 3, new Tree(null, 4, null))
 
 console.log(root)
 
-// 2
-function* iter_tree(tree) {
-    if (tree.left != null)
-        yield* iter_tree(tree.left)
-
-    yield tree.value
-
-    if (tree.right != null)
-        yield* iter_tree(tree.right)
-
-}
-
-for (v of iter_tree(root))
+for (v of root)
     console.log(v)
